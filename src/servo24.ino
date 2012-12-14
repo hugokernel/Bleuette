624787757effb0e74f53140f695b177265fa7514
hugokernel <hugo@digitalspirit.org>
2012-12-14 22:17:07 +0100

    Sequence src

--- src/servo24.ino
+++ src/servo24.ino
@@ -8,30 +8,52 @@ extern HardwareSerial Serial;
 unsigned long servos = 0;
 unsigned char values[14];
 
-typedef struct sequence_t {
+typedef struct motion_t {
     unsigned long servos;
     unsigned char count;        // Values count
     unsigned char values[14];
-} sequence;
+    unsigned long delay;
+}; // sequence;
 
-sequence_t seq_walk[2] = {
-    {
-        SERVO24_0 & SERVO24_1 & SERVO24_2,
-        3,
-        10, 60, 10
-    },
-    {
-        SERVO24_0 & SERVO24_1 & SERVO24_2,
-        3,
-        60, 10, 60
-    },
+typedef struct sequence_t {
+    bool loopable;
+    unsigned int count;
+    motion_t *motion;
 };
+
+motion_t motion_walk[] = {
+    {
+        SERVO24_0 & SERVO24_1 & SERVO24_2,
+        3,
+        { 10, 60, 10 },
+        3000
+    },
+    {
+        SERVO24_0 & SERVO24_1 & SERVO24_2,
+        3,
+        { 60, 10, 60 },
+        3000
+    }
+};
+
+sequence_t sequence_walk = {
+    true,
+    sizeof(motion_walk),
+    motion_walk
+};
+
+
+void runSequence(sequence_t seq) {
+
+}
 
 int main() {
   
     Servo24Hard servo = Servo24Hard(Serial, 1);
 
     while (1) {
+
+        //runSequence(sequence_walk);
 
 /*
         servos = SERVO24_0 & SERVO24_1 & SERVO24_2;
