6bcedb1284ae2867d58eed1a2e17c1b2e1e24e83
hugokernel <hugo@digitalspirit.org>
2013-06-30 18:46:37 +0000

    Servo controller dev

--- src/BleuettePi/Python/ServoController.py
+++ src/BleuettePi/Python/ServoController.py
@@ -4,20 +4,27 @@ import serial
 from time import sleep
 from array import array
 
-SERVO_0 = 1
-SERVO_1 = 2
-SERVO_2 = 4
-SERVO_3 = 8
-SERVO_4 = 16
-SERVO_5 = 32
-SERVO_6 = 64
-SERVO_7 = 128
-SERVO_8 = 256
-SERVO_9 = 512
-SERVO_10 = 1024
-SERVO_11 = 2048
-SERVO_12 = 4096
-SERVO_13 = 8192
+SERVO0 = 1
+SERVO1 = 2
+SERVO2 = 4
+SERVO3 = 8
+SERVO4 = 16
+SERVO5 = 32
+SERVO6 = 64
+SERVO7 = 128
+SERVO8 = 256
+SERVO9 = 512
+SERVO10 = 1024
+SERVO11 = 2048
+SERVO12 = 4096
+SERVO13 = 8192
+
+ANA0 = '0'
+ANA1 = '1'
+ANA2 = '2'
+ANA3 = '3'
+ANA4 = '4'
+ANA5 = '5'
 
 class ServoController:
 
@@ -25,11 +32,18 @@ class ServoController:
 
     HEADER = chr(255)
 
-    CMD_INIT      = 'I'
-    CMD_PAUSE     = 'P'
-    CMD_RESUME    = 'R'
-    CMD_CLEAR     = 'C'
-    CMD_SET       = 'S'
+    CMD_INIT    = 'I'   # Init
+    CMD_PAUSE   = 'P'   # Pause all servo
+    CMD_RESUME  = 'R'   # Resume from pause
+    CMD_CLEAR   = 'C'   # Clear all values
+    CMD_SET     = 'S'   # Set consigne
+
+    CMD_READAN0 = '0'   # Read analog value
+    CMD_READAN1 = '1'   # Read analog value
+    CMD_READAN2 = '2'   # Read analog value
+    CMD_READAN3 = '3'   # Read analog value
+    CMD_READAN4 = '4'   # Read analog value
+    CMD_READAN5 = '5'   # Read analog value
 
     DELAY_BYTE = 0.002
 
@@ -38,7 +52,7 @@ class ServoController:
 
     last_status_code = ''
 
-    values = array('c', ['a'] * SERVO_COUNT)
+    values = array('c', [chr(200)] * SERVO_COUNT)
 
     def __init__(self, device):
         self.com = serial.Serial(device, 9600, timeout = 1)
@@ -68,17 +82,17 @@ class ServoController:
             self.com.write(self.values[i])
 
             control += ord(self.values[i])
-#            print ord(self.values[i])
 
-        sleep(self.DELAY_BYTE)
+        #sleep(self.DELAY_BYTE)
         #print control % 255
-        print control % 255
-        self.com.write(chr(78)) #chr(control % 255))
+        #print control % 256
+        #self.com.write(chr(78)) #chr(control % 255))
+        self.com.write(chr(control % 256))
         
-        sleep(self.DELAY_BYTE)
+        #sleep(self.DELAY_BYTE)
 
         self.last_status_code = self.getResponse()
-        return self. getLastStatus()
+        return self.getLastStatus()
 
     def init(self):
         return self.command(self.CMD_INIT)
@@ -95,13 +109,22 @@ class ServoController:
     def clear(self):
         return self.command(self.CMD_CLEAR)
 
+    # Read analog value
+    def readAnalog(self, index):
+        self.com.write(self.HEADER)
+        sleep(self.DELAY_BYTE)
+        self.com.write(index)
+
+        #print index, ord(index), chr(index)
+
+        #self.last_status_code = self.getResponse()
+        return self.getResponse()
+        #return self.getLastStatus()
+
     def setValue(self, index, value):
         values[index] = value 
 
 '''
-    ServoController();
-    ServoController(HardwareSerial &);
-
     void setValue(unsigned char, unsigned char);
 
     unsigned char getPosition(unsigned char);
@@ -112,8 +135,6 @@ class ServoController:
 
     unsigned long get(unsigned char);
 
-    inline bool getLastStatus();
-
     bool command(SERVO_CMD, unsigned char [], unsigned char);
 '''
 
@@ -123,4 +144,5 @@ if __name__ == '__main__':
     print Servo.init()
     print Servo.sendValues()
     print Servo.resume()
+    print Servo.readAnalog(ANA2)
 
